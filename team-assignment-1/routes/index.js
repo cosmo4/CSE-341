df9a107a26bfd97c4814a769a76601123da32b92
cosmo4 <warner.luke0102@gmail.com>
2024-05-07 13:25:42 -0600

    ready for render, rest api

--- team-assignment-1/routes/index.js
+++ team-assignment-1/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const contactsRoute = require('./contacts')
 
-const baseController = require('../controllers/baseController');
 
 // GET /feed/posts
-router.get('/', baseController.getData);
+router.get('/contacts', contactsRoute);
 
 
 module.exports = router;
--- team-assignment-1/server.js
+++ team-assignment-1/server.js
@@ -1,24 +1,21 @@
 const express = require('express');
-const professionalRoute = require("./routes/professionalRoute")
+const professionalRoute = require("./routes")
+const contactsRoute = require('./routes/contacts')
 const baseController = require("./controllers/baseController")
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 
 const app = express();
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/professional', professionalRoute);
 
+app.use(bodyParser.json()).use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+.use('/contacts', contactsRoute);
 
 const port = 8080;
 
-// app.listen(process.env.port || port);
-// console.log('Web Server is listening at port ' + (process.env.port || port));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
